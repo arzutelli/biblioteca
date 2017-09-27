887b49548aef671f39f45979a78e1830e0b9207d
DanieleAn <DanieleAn@vt-danielean.valueteam.com>
2017-09-27 15:14:30 +0200

    update

--- src/test/java/com/nttdata/controller/LibroControllerTest.java
+++ src/test/java/com/nttdata/controller/LibroControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import com.nttdata.model.Libro;
 
 
+
 public class LibroControllerTest {
 	
 	private static final String baseUrl = "http://localhost:8080";
@@ -95,6 +96,48 @@ public class LibroControllerTest {
 	    	assertTrue(e.getStatusCode().equals(HttpStatus.NOT_FOUND));
 	    }
 		
+		
+		
+	}
+	
+	@Test
+	public void testPut() {
+		String URL = baseUrl+"/libro";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Libro request = new Libro();
+		request.setTitolo("Harry Potter");
+		request.setGenere("Fantasy");
+		request.setPrezzo(new BigDecimal("12.5"));
+		request.setScaffale("3");
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpEntity<Libro> entity = new HttpEntity<Libro>(request ,headers);
+		
+		Libro libro = restTemplate.postForObject(URL, entity , Libro.class);
+		
+		assertTrue(libro!=null);
+		assertTrue(libro.getIdLibro() != 0);
+		
+		libro.setTitolo("Manny Potter");
+		libro.setGenere("fantascienza");
+		libro.setPrezzo(new BigDecimal("12.5"));
+		libro.setScaffale("4");
+		
+		HttpHeaders headersPut = new HttpHeaders();
+		headersPut.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpEntity<Libro> entityPut = new HttpEntity<Libro>(libro ,headersPut);
+		
+		restTemplate.put(URL+"/"+libro.getIdLibro(),  entityPut , Libro.class);
+		
+		Libro usernew = restTemplate.getForObject(URL+"/"+libro.getIdLibro(), Libro.class);
+		assertTrue(usernew!=null);
+		
+		assertTrue(usernew.equals(libro));
 		
 		
 	}
