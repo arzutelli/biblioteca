8680d67cb863caf8dfe3f14255771a606b120314
arzucastilma <arzucastilma@vt-arzucastilma.valueteam.com>
2017-09-28 19:21:49 +0200

    aggiunti ultimi commenti

--- src/test/java/com/nttdata/controller/UserControllerTest.java
+++ src/test/java/com/nttdata/controller/UserControllerTest.java
@@ -25,7 +25,7 @@ public class UserControllerTest {
 	private static final String baseUrl = "http://localhost:8080";
 	
 
-	@Test
+	@Test // indica che il metodo è un Test di JUnit (Framework JUnit)
 	public void test() {
 		String URL = baseUrl+"/user";
 		
@@ -33,6 +33,10 @@ public class UserControllerTest {
 		
 		User[] users = restTemplate.getForObject(URL, User[].class);
 		
+		/**
+		 * asserzioni per la verifica dell'esito del test
+		 * se anche una sola asserzione è false il test è fallito
+		 */
 		assertTrue(users!=null);
 		assertTrue(users.length > 0);
 		
@@ -45,25 +49,45 @@ public class UserControllerTest {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		
-		User request = new User();
-		request.setSurname("Thomas");
-		request.setName("Loesch");
-		request.setEmail("thomas.loesch@nttdata.com");
-		request.setDataNascita(new Date());
+		User utenteInput = new User();
+		utenteInput.setSurname("Thomas");
+		utenteInput.setName("Loesch");
+		utenteInput.setEmail("thomas.loesch@nttdata.com");
+		utenteInput.setDataNascita(new Date());
 		
+		/**
+		 * metodo per creare l'header per il content-type:Application-json
+		 * necessario per la corretta lettura della request
+		 */
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		
-		HttpEntity<User> entity = new HttpEntity<User>(request ,headers);
+		/**
+		 * creazione della request comprensiva di body (utenteInput) e headers
+		 */
+		HttpEntity<User> request = new HttpEntity<User>(utenteInput ,headers);
 		
-		User user = restTemplate.postForObject(URL, entity , User.class);
+		/**
+		 * invocazione del metodo post sulla risorsa identificata da URL
+		 * con User come tipo di ritorno della risposta
+		 */
+		User user = restTemplate.postForObject(URL, request , User.class);
 		
 		assertTrue(user!=null);
 		assertTrue(user.getBadgeId() != 0);
 		
+		/**
+		 * ricerca dell'utente appena inserito per successiva veifica di corretto inserimento
+		 */
 		User userGet = restTemplate.getForObject(URL+"/"+user.getBadgeId(), User.class);
 		assertTrue(userGet!=null);
 		
+		/**
+		 * è stato necessario effettuare l'override del metodo equals per fare in modo
+		 * che gli oggetti siano uguali anche se sono istanze diverse 
+		 * è stata ignorata volutamente la property eta perchè calcolata a runtime solo 
+		 * in risposta al metodo get e non alla post
+		 */
 		assertTrue(userGet.equals(user));
 		
 	}
